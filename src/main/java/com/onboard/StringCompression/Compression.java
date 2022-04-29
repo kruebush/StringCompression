e1cef73f4a2b76ecc21a3875355a62de23b50aee
Kami Ruebush <kami691@revature.net>
2022-04-29 11:10:35 -0500

    Completed

---
+++ src/main/java/com/onboard/StringCompression/Compression.java
@@ -0,0 +1,37 @@
+package com.onboard.StringCompression;
+
+import org.w3c.dom.css.CSSRule;
+
+/**
+ * Hello world!
+ *
+ */
+public class Compression 
+{
+    public static void main( String[] args )
+    {
+        Compression comp = new Compression();
+        System.out.println(comp.compress("aabccccaaaa"));
+        System.out.println(comp.compress("abcdef"));
+    }
+    public static String compress(String str) {
+    	StringBuilder compressedString = new StringBuilder();
+    	int repeatCount = 0;
+    	for (int i =0; i < str.length(); i++) {
+    		repeatCount++;
+    		
+    		if (str.length() <= i+1 || str.charAt(i) !=str.charAt(i+1)) {
+    			compressedString.append(str.charAt(i));
+    			compressedString.append(repeatCount);
+    			repeatCount=0;
+    		}
+    	}
+    	
+    	if (compressedString.length() < str.length()) {
+    		return compressedString.toString();
+    	}
+    	else {
+    		return str;
+    	}
+    }
+}
---
+++ src/test/java/com/onboard/StringCompression/CompressionTest.java
@@ -0,0 +1,38 @@
+package com.onboard.StringCompression;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CompressionTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CompressionTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CompressionTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
